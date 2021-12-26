3977c53af3769fcacea10f5f9823227cfb696b7e
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2021-12-26 16:17:35 -0600

    Add a new pages controller with his view

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+
+  # This method will trigger the home.html.erb who is located in app/views/pages/home.html.erb .This file will display the html using the .erb (embedded ruby) syntax
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Here we can ommit the _controller.rb , rails already know where the file is located.
+  root 'pages#home' # This send to the pages_controller -> app/controlllers/application_controller.rb
+  # The keyword after the '#' means the method we will trigger; in this case is the  home method
 end
